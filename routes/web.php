d559bfc367bca7d36ff81341f12a5f513c40816d
jgaro-cas <jerome.garo@gmail.com>
2020-10-01 19:38:29 +0000

    Models routes et controleurs vides

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\ProjectController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('tasks', TaskController::class);
+Route::resource('projects', ProjectController::class);
